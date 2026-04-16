df07cf33aa1a6b74ed5bccb76e537805cbde40aa
Junta <86794511+dungta1610@users.noreply.github.com>
2026-04-16 21:43:17 +0700

    finish practice 6: ack, nack, reject, requeue

---
+++ Practice 6/sender/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %v", msg, err)
+	}
+}
+
+func bodyFromArgs(args []string) string {
+	if len(args) < 2 {
+		return "ok: default message"
+	}
+	return strings.Join(args[1:], " ")
+}
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "failed to open channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"task_queue_v6",
+		true,  // durable
+		false, // auto-delete
+		false, // exclusive
+		false, // no-wait
+		nil,
+	)
+	failOnError(err, "failed to declare queue")
+
+	body := bodyFromArgs(os.Args)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = ch.PublishWithContext(
+		ctx,
+		"",
+		q.Name,
+		false,
+		false,
+		amqp.Publishing{
+			ContentType:  "text/plain",
+			DeliveryMode: amqp.Persistent,
+			Body:         []byte(body),
+		},
+	)
+	failOnError(err, "failed to publish message")
+
+	log.Printf("[x] Sent: %s", body)
+}
