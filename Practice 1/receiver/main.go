b90ca73b898e36ea8666fa60fc5ffcc95ab2807f
Junta <86794511+dungta1610@users.noreply.github.com>
2026-04-16 10:02:28 +0700

    first commit

---
+++ Practice 1/receiver/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"log"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %v", msg, err)
+	}
+}
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"hello2",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	msgs, err := ch.Consume(
+		q.Name,
+		"",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	var forever chan struct{}
+
+	go func() {
+		for d := range msgs {
+			log.Printf("[x] Received: %s", d.Body)
+			if err := d.Ack(false); err != nil {
+				log.Printf("Ack error: %v", err)
+			}
+		}
+	}()
+
+	log.Println("[*] Waiting for messages. Press CTRL+C to exit")
+	<-forever
+}
